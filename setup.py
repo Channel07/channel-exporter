6c621108952b3363efe76db212a80698610a05c6
竹永康 <zhuyk@project-ec.com>
2025-04-03 15:33:13 +0800

    1.1
    1.正式发布版本 `1.1` 。

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if sys.version_info.major < 3:
 
 setuptools.setup(
     name='channel-exporter',
-    version='1.1alpha1',
+    version='1.1',
     author='Unnamed great master',
     author_email='<gqylpy@outlook.com>',
     license='MIT',
